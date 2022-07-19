77e37f797b4c4186f0c9d41ee0dc1e46e024a787
gergely.eory <gergely.eory@solirius.com>
2022-07-19 14:14:29 +0100

    updated angular version, added linting

--- jobs-list/src/app/components/listing-card/listing-card.component.ts
+++ jobs-list/src/app/components/listing-card/listing-card.component.ts
@@ -2,12 +2,10 @@ import { Component, Input } from '@angular/core';
 import { JobListing } from '../../models/listing';
 
 @Component({
-  selector: 'app-listing-card',
-  templateUrl: './listing-card.component.html',
-  styleUrls: ['./listing-card.component.scss']
+	selector: 'app-listing-card',
+	templateUrl: './listing-card.component.html',
+	styleUrls: ['./listing-card.component.scss'],
 })
 export class ListingCardComponent {
-
-  @Input() listing!: JobListing;
-
+	@Input() listing!: JobListing;
 }
--- jobs-list/src/app/pages/listings-page/listings-page.component.ts
+++ jobs-list/src/app/pages/listings-page/listings-page.component.ts
@@ -2,15 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ListingsApiService } from '../../services/listings-api.service';
 
 @Component({
-  templateUrl: './listings-page.component.html',
-  styleUrls: ['./listings-page.component.scss']
+	templateUrl: './listings-page.component.html',
+	styleUrls: ['./listings-page.component.scss'],
 })
 export class ListingsPageComponent implements OnInit {
+	constructor(public listingsApi: ListingsApiService) {}
 
-  constructor(public listingsApi: ListingsApiService) { }
-
-  ngOnInit(): void {
-    this.listingsApi.loadListings().subscribe(listings => console.log(listings));
-  }
-
+	ngOnInit(): void {
+		this.listingsApi.loadListings().subscribe((listings) => console.log(listings));
+	}
 }
